ad798361fafd4b2b44f4adc6bc3572c5e7623960
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-05-31 09:51:10 +0900

    コマンド出力

--- silver49.rb
+++ silver49.rb
@@ -4,3 +4,13 @@ p $1
 p $2
 p $3
 p $4
+
+# 文字クラス
+p /ruby/ === "Ruby"
+p /ruby/i === "Ruby"
+p /a.*b/ === "a\nb"
+p /a.*b/m === "a\nb"
+
+# コマンド出力
+puts `date +%Y/%m/%d`  # 今日の日付が出力されるはず ※今回はMacなのでこちら Windowsの場合は下
+# puts `date /T`
