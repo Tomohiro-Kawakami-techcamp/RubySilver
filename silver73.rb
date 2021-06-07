a25366e213ad8de725b20a2b46072139d64ca66f
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-07 13:35:47 +0900

    7日まで

--- silver73.rb
+++ silver73.rb
@@ -8,3 +8,14 @@ end
 
 c1 = C1.new
 p c1 == c1.method1
+
+# def式中におけるdef式の呼び出し
+class C2
+  def method1
+    def method2
+    end
+  end
+end
+p C2.instance_methods(false)
+p C2.new.method1
+p C2.instance_methods(false)
