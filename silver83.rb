483819e040090478cfb52f80a109adbe4fce6854
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-10 10:12:17 +0900

    サブクラス

--- silver83.rb
+++ silver83.rb
@@ -12,3 +12,11 @@ end
 
 p Baz2.new.public_method1  # => nil
 p Baz2.new.public_method2  # => NoMethodError
+
+# サブクラスにおける可視性の変更
+class Baz2Ext < Baz2
+  public :private_method1
+end
+
+p Baz2.new.private_method1  # => NoMethodError
+p Baz2Ext.new.private_method1  # => nil
