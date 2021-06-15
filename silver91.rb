a39514e176be7d45669bedde2eaca1a1b33abe23
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-15 10:54:35 +0900

    オブジェクトの比較

--- silver91.rb
+++ silver91.rb
@@ -7,3 +7,18 @@ p "foo".object_id
 p "foo".object_id # 異なるIDになる
 p :foo.object_id
 p :foo.object_id
+
+p "foo".class
+p :foo.class
+
+a = "foo"
+p a
+p a.hash
+p a.object_id
+b = "foo"
+p b
+p b.hash
+p b.object_id
+p a.eql?(b)
+p a.equal?(b)
+p a == b
