0682380d27805815e4cd2c52cc3fbb2d417e381d
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-16 10:28:32 +0900

    未定義のメソッド

--- silver93.rb
+++ silver93.rb
@@ -10,3 +10,14 @@ p f.instance_variables
 p f.instance_variable_get(:@hoge)
 p f.instance_variable_set(:@hoge, 2)
 p f.instance_variable_get(:@hoge)
+
+# 未定義のメソッドの呼び出し
+class Bar
+  def method_missing(name, *args)
+    puts name
+  end
+end
+
+p b = Bar.new
+p b.hoge
+# p "string".hoge => NoMethodError
