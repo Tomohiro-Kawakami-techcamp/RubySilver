899b6d09207b3bc0537772debd82d835f4a03bbf
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-16 10:42:18 +0900

    数値繰り返しまで

--- silver94.rb
+++ silver94.rb
@@ -15,3 +15,15 @@ p hoge.inspect
 
 # 数値の切り捨て、切り上げ
 p 1.9.ceil
+p 1.9.floor
+p 1.9.truncate
+p -1.1.ceil
+p -1.1.floor
+p -1.1.round
+p -1.1.truncate
+
+# 数値の絶対値
+p -3.abs
+
+# 数値を使った繰り返し
+1.step(100, 2){|n| puts n}
