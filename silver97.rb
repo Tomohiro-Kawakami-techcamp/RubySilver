83f18261133010402c8a3faf201f056110eaafc5
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-17 10:29:23 +0900

    複素数

---
+++ silver97.rb
@@ -0,0 +1,16 @@
+# Rationalクラスのインスタンスと異なるクラスのインスタンスの四則演算
+p Rational(1, 2) + Rational(1, 3)
+p Rational(1, 2) + 1
+p Rational(1, 2) + 0.25
+
+# 複素数リテラル
+a = (1 + 2i)
+p a
+
+# 複素数の実部と虚部
+a = (1 + 2i)
+p a
+p a.real
+p a.imaginary
+p 3.real
+p 3.imaginary
