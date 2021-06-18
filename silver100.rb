8631fb8e3b284072420e470aceebd808c26d194b
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-18 10:44:12 +0900

    文字列で指定

--- silver100.rb
+++ silver100.rb
@@ -13,3 +13,12 @@ p 'abcdefg'[-2, 3]
 a = 'abcdefg'
 p a.slice!(1, 3)
 puts a
+
+# 文字列で指定する
+p 'abcdefg'["bc"]
+p 'abcdefg'.slice("bc")
+p 'abcdefg'["bd"]
+a = 'abcdefg'
+p a
+p a.slice!("bc")
+puts a
