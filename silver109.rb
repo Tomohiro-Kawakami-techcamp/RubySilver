600ec8a6278f5e99998f733a60f1d700a5a36c6c
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-24 11:57:42 +0900

    配列の要素参照

--- silver109.rb
+++ silver109.rb
@@ -10,3 +10,15 @@ a = [1, 2, 3]
 p a.object_id
 p a.replace([4, 5, 6])
 p a.object_id
+
+# 配列の要素を参照する(1)
+a = [1, 2, 3]
+p a[1]
+p a.at(1)
+p a[1..2]
+p a.values_at(1)
+
+# 配列の要素を参照する(2)
+a = [1, 2, 3]
+p a
+# p a.fetch(4) => IndexError
