715ed38b99ee08a459a449c3330762e4a16b6ce9
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-24 11:30:36 +0900

    配列の生成

--- silver107.rb
+++ silver107.rb
@@ -10,3 +10,20 @@ p a
 p a.object_id
 p b = :abc
 p b.object_id
+
+# 配列の生成
+a = [1, 2, 3]
+p a
+p a.class
+
+a = Array[1, 2, 3]
+p a
+
+a = Array.new(3, "str")
+p a
+
+a = Array.new([1, 2, 3])
+p a
+
+a = Array.new(3){|i| i * 3}
+p a
