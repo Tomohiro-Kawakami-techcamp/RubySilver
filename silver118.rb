dce7b31d9f0f047d7a3d1a2a9caaa9b332456aff
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-06-29 11:26:39 +0900

    ハッシュを使った繰り返し

--- silver118.rb
+++ silver118.rb
@@ -13,4 +13,15 @@ p a.key?("orange")
 # ハッシュを調べる(3)
 a = {"apple" => "fruit", "coffee" => "drink"}
 p a.value?("fruit")
-p a.keys?("foods")
+p a.key?("foods")
+
+# ハッシュを使った繰り返し(1)
+a = {"apple" => "fruit", "coffee" => "drink"}
+p a
+a.each{|key, value| puts "#{key} => #{value}\n"}
+
+# ハッシュを使った繰り返し(2)
+a = {"apple" => "fruit", "coffee" => "drink"}
+p a
+a.each_key{|key| puts "key: => #{key}\n"}
+a.each_value{|value| puts "value: => #{value}\n"}
